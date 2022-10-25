0f3524171323097321f80a9a767723d63c424dd2
vucina1906 <vucina19931906@gmail.com>
2022-10-25 11:44:13 +0200

    Finishing web app

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 #load pickle model
 model = pickle.load(open("BHP.pickle","rb"))
 scaler = pickle.load(open("scaler.pickle","rb"))
@@ -31,6 +31,15 @@ def predict_api():
     print(result[0])
     return jsonify(result[0])
 
+@app.route("/predict",methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "The predicted price is {}".format(output))
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
     
